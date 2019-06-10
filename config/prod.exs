b2f992f48e763e90eba055dc34cceb2614e1d393
Syaiful Sabril <syaiful.sabril@gmail.com>
2019-06-10 09:16:18 +0700

    fix

--- config/prod.exs
+++ config/prod.exs
@@ -10,6 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :chat, ChatWeb.Endpoint,
+  http: [port: {:system, "PORT"}],
   load_from_system_env: true,
   url: [scheme: "https", host: "elixir-chat-app-ex.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
